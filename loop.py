7545ae7b2db61381c96b3f3b8f3c8cafea9ea2d7
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-11 21:41:17 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,55 @@
+# a=1,2,3,4,5
+# print(*a)        1 2 3 4 5
+# a=1,2,3
+# for josh in a:
+#     print(josh,end=" ")    1 2 3 
+# for i in range(5):    
+#     print(i)          0
+#                       1
+#                       2
+#                       3
+                    #   4
+# n=int(input("enter the value of n:"))
+# for i in range(1,n+1):
+#     print(i,end="\n")enter the value of n:5
+#                      1
+#                      2
+#                      3
+#                      4
+#                      5
+# for i in range(1,30,2):
+#     print(i,end=' ')     1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 
+# for i in range(2,30,2):
+#     print(i,end=' ')     2 4 6 8 10 12 14 16 18 20 22 24 26 28 
+# for i in range(10,0,-1):
+#     print(i)
+# for i in range(10,-1,-1):   (it print from 10 to 0 vertically)
+#     print(i)
+# n=[1,4.5,'python']    python
+# for i in n[::-1]:   4.5
+#     print(i)         1
+# v='programming'      (print programming skipping by one vertically)
+# for e in v[::2]:
+#     print(e)
+# n=int(input("enter any integer Number:"))    enter any integer Number:4
+# for i in range(n):     (3 times 0 print vertically.)
+#     print(i)            the loop has been executed.
+#     print(i)           (3 times 1 print vertically. )
+#     print(i)           the loop has been executed.
+#     print("the loop has been executed.")(3 times 2 print vertically.)
+# print("Done")                    (3 times 3 print vertically.)       
+# n=int(input("enter any integer number"))
+# for i in range(25,n):   #enter any integer number2
+#     print(i,end=" ") 
+# n=int(input("enter any integer number:"))
+# for i in range(2,n,2):       enter any integer number:4
+#     print(i)                 2
+# b=int(input("enter any integer number:"))
+# if(b%2==0):
+#     print(f"{b] even number")
+# else:
+#     print(f"{b} odd number")    enter any integer number:4
+# for i in range(1,5):    1
+#     print(i*i)          4
+#                         9
+#                         16
